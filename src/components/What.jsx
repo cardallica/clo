d5925a56e03f561d895d68e97f6739a6534ec2b9
carlo cardani <carlo.cardani@chili.com>
2018-09-19 10:45:14 +0200

    Style bullets

--- src/components/What.jsx
+++ src/components/What.jsx
@@ -24,6 +24,9 @@ export default class WhatPage extends React.Component {
 					<BulletPortrait image={AnsiaPng} text="sostegno in casi di infertilità e nella PMA"/>
                     <BulletPortrait image={ConflittiPng} text="psicoterapia EMDR"/>
 				</div>
+				<p className="pageText">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+				</p>
 			</div>
 		);
 	}
